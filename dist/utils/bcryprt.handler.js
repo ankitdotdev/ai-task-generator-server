c65b90b73e797bc99d16c044f41536850d1543c8
ankit00010 <ankitmish83@gmail.com>
2026-02-16 22:42:33 +0530

    feat: replace bcrypt with bcryptjs for password hashing and comparison

--- dist/utils/bcryprt.handler.js
+++ dist/utils/bcryprt.handler.js
@@ -4,15 +4,15 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.bcryptCompareHashPassword = exports.bcryptHashPassword = void 0;
-const bcrypt_1 = __importDefault(require("bcrypt"));
+const bcryptjs_1 = __importDefault(require("bcryptjs"));
 // Hashes a plain-text password before storing it in the database
 // Uses salt rounds = 10 (balanced choice between security and performance)
 const bcryptHashPassword = (password) => {
-    return bcrypt_1.default.hash(password, 10);
+    return bcryptjs_1.default.hash(password, 10);
 };
 exports.bcryptHashPassword = bcryptHashPassword;
 // Compares a plain-text password with the stored hashed password during login
 const bcryptCompareHashPassword = (password, hashPassword) => {
-    return bcrypt_1.default.compare(password, hashPassword);
+    return bcryptjs_1.default.compare(password, hashPassword);
 };
 exports.bcryptCompareHashPassword = bcryptCompareHashPassword;
